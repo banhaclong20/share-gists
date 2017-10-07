f6e489f55d1c565af89d4865cf1406dd7d9a1534
Tom Tran <ttran@xpressdocs.com>
2017-10-06 19:42:10 -0500

    Updated UI

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,21 @@ router.get('/google', passport.authenticate('google', {scope: ['profile','email'
 
 // Use Authenticate Request callback
 router.get('/google/callback', 
-    passport.authenticate('google', {failureRedirect: '/'}), 
-    (req, res) => {
-    // Redirect Home after successful  authentication
+    passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/dashboard');
-}) 
+}); 
+
+router.get('/verify', (req, res) => {
+    if(req.user) {
+        console.log(req.user)
+    } else {
+        console.log('not auth');
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
